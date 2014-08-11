46fa3642e776994370cbdfa79df8a7e62b316076
Sergey Zolotarev <sryze@yandex.com>
2014-08-12 02:22:19 +0700

    Fix coding style

--- main.js
+++ main.js
@@ -1,77 +1,77 @@
 define(function(reuire, exports, module) {
-    "use strict";
+    'use strict';
     
-    var LanguageManager = brackets.getModule("language/LanguageManager");
-    var CodeMirror = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
+    var LanguageManager = brackets.getModule('language/LanguageManager');
+    var CodeMirror = brackets.getModule('thirdparty/CodeMirror2/lib/codemirror');
     
-    CodeMirror.defineMode("cmake", function(config) {
+    CodeMirror.defineMode('cmake', function (config) {
         var keywords = {
-            "if": [
-                "NOT","AND", "OR", "COMMAND", "POLICY", "TARGET",
-                "EXISTS", "IS_NEWER_THAN", "IS_DIRECTORY", "IS_SYMLINK",
-                "IS_ABSOLUTE", "MATCHES", "LESS", "GREATER", "EQUAL",
-                "STRLESS", "STRGREATER", "STREQUAL", "VERSION_LESS",
-                "VERSION_EQUAL", "VERSION_GREATER", "DEFINED"
+            'if': [
+                'NOT','AND', 'OR', 'COMMAND', 'POLICY', 'TARGET',
+                'EXISTS', 'IS_NEWER_THAN', 'IS_DIRECTORY', 'IS_SYMLINK',
+                'IS_ABSOLUTE', 'MATCHES', 'LESS', 'GREATER', 'EQUAL',
+                'STRLESS', 'STRGREATER', 'STREQUAL', 'VERSION_LESS',
+                'VERSION_EQUAL', 'VERSION_GREATER', 'DEFINED'
             ]
         };
 
-        var COMMENT  = { name: "COMMENT",  regexp: /#/ };
-        var QUOTE    = { name: "QUOTE",    regexp: /"/ };
-        var OBRACKET = { name: "OBRACKET", regexp: /\[([=]*)\[/ };
-        var CBRACKET = { name: "CBRACKET", regexp: /\]([=]*)\]/ };
-        var SPACE    = { name: "SPACE",    regexp: /\s+/ };
-        var LPAREN   = { name: "LPAREN",   regexp: /\(/ };
-        var RPAREN   = { name: "RPAREN",   regexp: /\)/ };
-        var IDENT    = { name: "IDENT",    regexp: /[A-Za-z_][A-Za-z0-9_]*/ };
-        var VARREF   = { name: "VARREF",   regexp: /\$(ENV)?{.*?}/ };
-        var ARGUMENT = { name: "ARGUMENT", regexp: /[^()#"\\]+/ };
+        var COMMENT  = { name: 'COMMENT',  regexp: /#/ };
+        var QUOTE    = { name: 'QUOTE',    regexp: /"/ };
+        var OBRACKET = { name: 'OBRACKET', regexp: /\[([=]*)\[/ };
+        var CBRACKET = { name: 'CBRACKET', regexp: /\]([=]*)\]/ };
+        var SPACE    = { name: 'SPACE',    regexp: /\s+/ };
+        var LPAREN   = { name: 'LPAREN',   regexp: /\(/ };
+        var RPAREN   = { name: 'RPAREN',   regexp: /\)/ };
+        var IDENT    = { name: 'IDENT',    regexp: /[A-Za-z_][A-Za-z0-9_]*/ };
+        var VARREF   = { name: 'VARREF',   regexp: /\$(ENV)?{.*?}/ };
+        var ARGUMENT = { name: 'ARGUMENT', regexp: /[^()#"\\]+/ };
 
-        COMMENT.handle = function(stream, state, token) {
+        COMMENT.handle = function (stream, state, token) {
             if (token.matchRule(OBRACKET)) {
                 state.inBracketComment = true;
                 state.bracketLength = token.match[0].length;
-                token.class = "comment";
+                token.class = 'comment';
             } else {
                 stream.skipToEnd();
-                token.class = "comment";
+                token.class = 'comment';
             }
         }
 
-        QUOTE.handle = function(stream, state, token) {
+        QUOTE.handle = function (stream, state, token) {
             state.inString = true;
-            token.class = "string";
+            token.class = 'string';
         }
 
-        LPAREN.handle = function(stream, state, token) {
+        LPAREN.handle = function (stream, state, token) {
             state.parenBalance++;
         }
 
-        RPAREN.handle = function(stream, state, token) {
+        RPAREN.handle = function (stream, state, token) {
             state.parenBalance--;
             if (state.parenBalance == 0) {
                 state.command = null;
             }
         }
 
-        IDENT.handle = function(stream, state, token) {
+        IDENT.handle = function (stream, state, token) {
             if (state.command) {
                 var keyword = stream.current();
                 if (keywords.hasOwnProperty(state.command)
                     && keywords[state.command].indexOf(keyword) >= 0) {
-                    token.class = "keyword";
+                    token.class = 'keyword';
                 }
             } else {
                 stream.eatSpace();
                 if (token.matchRule(LPAREN)) {
                     stream.backUp(1);
                     state.command = stream.current();
-                    token.class = "keyword";
+                    token.class = 'keyword';
                 }
             }
         }
 
-        VARREF.handle = function(stream, state, token) {
-            token.class = "variable";
+        VARREF.handle = function (stream, state, token) {
+            token.class = 'variable';
         }
 
         var rules = [
@@ -80,7 +80,7 @@ define(function(reuire, exports, module) {
         ];
         
         return {
-            token: function(stream, state) {
+            token: function (stream, state) {
                 var token = {
                     rule: null,
                     class: null,
@@ -95,14 +95,14 @@ define(function(reuire, exports, module) {
                 };
                 
                 if (state.inString) {
-                    token.class = "string";
+                    token.class = 'string';
                     if (token.matchRule(QUOTE)) {
                         state.inString = false;
                     } else {
                         stream.next();
                     }
                 } else if (state.inBracketComment) {
-                    token.class = "comment";
+                    token.class = 'comment';
                     if (token.matchRule(CBRACKET)
                         && token.match[0].length == state.bracketLength) {
                         state.inBracketComment = false;
@@ -111,7 +111,7 @@ define(function(reuire, exports, module) {
                     }
                 } else {               
                     rules.every(function(r) {
-                        if (token.matchRule(r) && r.hasOwnProperty("handle")) {
+                        if (token.matchRule(r) && r.hasOwnProperty('handle')) {
                             r.handle(stream, state, token);
                         }
                         return !token.match;
@@ -121,7 +121,7 @@ define(function(reuire, exports, module) {
                 return token.class;
             },
             
-            startState: function() {
+            startState: function () {
                 return {
                     inString: false,
                     inBracketComment: false,
@@ -131,17 +131,17 @@ define(function(reuire, exports, module) {
                 };
             },   
 
-            lineComment: "#"
+            lineComment: '#'
         };
     });
 
-    CodeMirror.defineMIME("text/x-cmake", "cmake");
+    CodeMirror.defineMIME('text/x-cmake', 'cmake');
     
-    LanguageManager.defineLanguage("cmake", {
-        name: "CMake",
-        mode: "cmake",
-        fileExtensions: ["cmake", "cmake.in"],
-        fileNames: ["CMakeLists.txt"],
-        lineComment: ["#"]
+    LanguageManager.defineLanguage('cmake', {
+        name: 'CMake',
+        mode: 'cmake',
+        fileExtensions: ['cmake', 'cmake.in'],
+        fileNames: ['CMakeLists.txt'],
+        lineComment: ['#']
     });
 });
