7177d08a6b6730ae17e8178b32a29397aabf40f0
Sergey Zolotarev <sryze@yandex.com>
2014-09-06 06:07:41 +0700

    Quotes again

--- commands.js
+++ commands.js
@@ -1,15 +1,15 @@
 (function () {
-    var fs = require('fs');
+    var fs = require("fs");
 
-    var options = { encoding: 'utf-8' };
-    var text = fs.readFileSync('commands.txt', options);
+    var options = { encoding: "utf-8" };
+    var text = fs.readFileSync("commands.txt", options);
     var keywords = {};
     var commands = text.match(/[a-zA-Z_][a-zA-Z0-9_]*(?=\r?\n[-]+)/gm);
 
     commands.forEach(function (cmd) {
-        var pattern = '::\\r?\\n\\r?\\n ' + cmd
-                    + '\([\\s\\S]*?\)(?=\\r?\\n\\r?\\n)';
-        var declarations = text.match(new RegExp(pattern, 'gm')) || [];
+        var pattern = "::\\r?\\n\\r?\\n " + cmd
+                    + "\([\\s\\S]*?\)(?=\\r?\\n\\r?\\n)";
+        var declarations = text.match(new RegExp(pattern, "gm")) || [];
         var args = [];
         declarations.forEach(function (decl) {
             args = args.concat(decl.match(/\b[A-Z_][A-Z0-9_]*\b/g) || []);
@@ -19,5 +19,5 @@
         }).sort();
     });
 
-    fs.writeFileSync('commands.json', JSON.stringify(keywords), options);
+    fs.writeFileSync("commands.json", JSON.stringify(keywords), options);
 })();
